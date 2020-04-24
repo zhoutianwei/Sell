1b07e1afabb1c447d03bec1c1fe14db3f0699715
zhou <A649024921@gmail.com>
2020-04-24 17:00:12 +0900

    init

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <hello-world></hello-world>
   </div>
 
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
 export default {
-  name: 'App'
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
