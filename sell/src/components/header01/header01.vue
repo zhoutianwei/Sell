ae77fa842cc04167f8428acdbdee6857df9bec12
zhou <A649024921@gmail.com>
2020-04-29 19:23:24 +0900

    header finished

--- sell/src/components/header01/header01.vue
+++ sell/src/components/header01/header01.vue
@@ -17,9 +17,17 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="supports-count">
+          <span class="count">{{seller.supports.length}}个 <font-awesome-icon icon="angle-right" style="margin-left: 2px; font-size: 10px;"/></span>
+      </div>
     </div>
     <div class="bulletin-wrapper">
-
+      <span class="bulletin-title"></span><span
+class="bulletin-text">{{seller.bulletin}}</span>
+      <font-awesome-icon class="angle-right-style" icon="angle-right"/>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
     </div>
   </div>
 </template>
@@ -41,9 +49,12 @@ export default {
 <style lang="stylus" scoped>
   @import "../../common/stylus/mixin.styl"
   .header
-    background: black
+    position: relative
+    overflow: hidden
     color: aliceblue
+    background: rgba(7,17,27,0.5)
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -96,5 +107,55 @@ export default {
           .text
             line-height: 12px
             font-size: 10px
+
+      .supports-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0,0,0,0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: 10px
+
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7,17,27,0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 4px
+        font-size: 10px
+      .angle-right-style
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 
 </style>
