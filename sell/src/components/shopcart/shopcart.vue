a57af11ad334142ed6e2665a0035249799b261b9
zhou <A649024921@gmail.com>
2020-05-06 17:12:06 +0900

    shopcart

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -3,16 +3,17 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <font-awesome-icon icon="shopping-cart" class="icon-shopping-cart"></font-awesome-icon>
+          <div class="logo" :class="{'highlight': totalCount>0}">
+            <font-awesome-icon icon="shopping-cart" class="icon-shopping-cart" :class="{'highlight': totalCount>0}"></font-awesome-icon>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">0円</div>
-        <div class="desc">配送費￥{{deliveryPrice}}円</div>
+        <div class="price" :class="{'highlight': totalCount>0}">￥{{totalPrice}}円</div>
+        <div class="desc">配送手数料￥{{deliveryPrice}}円</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥ {{minPrice}} 円から配送
+        <div class="pay" :class="payClass">
+          {{payDesc}}
         </div>
       </div>
     </div>
@@ -22,8 +23,55 @@
 <script>
     export default {
         props: {
+            selectFoods: {
+              type: Array,
+              default () {
+                  return [
+                      {
+                          price: 20,
+                          count: 1
+                      }
+                  ]
+              }
+            },
             deliveryPrice: Number,
             minPrice: Number
+        },
+        computed: {
+            totalPrice () {
+                let total = 0
+                this.selectFoods.forEach((food) => {
+                    total += food.price * food.count
+                })
+                return total
+            },
+            totalCount () {
+                let count = 0
+                this.selectFoods.forEach((food) => {
+                    count += food.count
+                })
+                return count
+            },
+            payDesc () {
+                // eslint-disable-next-line no-irregular-whitespace
+              if (this.totalPrice === 0)　{
+                  // eslint-disable-next-line no-irregular-whitespace
+                  return '￥'　+ this.minPrice　+ '円から配送'
+              } else if (this.totalPrice < this.minPrice) {
+                  let diff = this.minPrice - this.totalPrice
+                  return `あと￥${diff}円で配送`
+              } else {
+                  return 'カートを見る'
+              }
+            },
+            payClass () {
+              if (this.totalPrice < this.minPrice) {
+                  return 'not-enough'
+              } else {
+                  return 'enough'
+              }
+            }
+
         }
     }
 </script>
@@ -40,6 +88,7 @@
       display: flex
       background: #141d27
       font-size: 0
+      color: rgba(255,255,255,0.4)
       .content-left
         flex: 1
         .logo-wrapper
@@ -60,10 +109,28 @@
             border-radius: 50%
             text-align: center
             background: #2b343c
+            &.highlight
+              background: rgb(0,160,220)
             .icon-shopping-cart
               margin-top: 8px
               font-size: 24px
               color: #80858a
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            color: #fff
+            background: rgb(240, 20, 20)
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
         .price
           display : inline-block
           vertical-align: top
@@ -74,15 +141,27 @@
           border-right: 1px solid rgba(255,255,255,0.1)
           font-size: 16px
           font-weight: 700
-          color: rgba(255,255,255,0.4)
+          &.highlight
+            color: #fff
         .desc
           display: inline-block
           vertical-align: top
           margin: 12px 0 0 12px
           line-height: 24px
-          color: rgba(255,255,255,0.4)
           font-size: 10px
       .content-right
         flex: 0 0 105px
         width: 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          font-weight: 700
+          &.not-enough
+            background: #2b333b
+          &.enough
+            background: #00b43c
+            color: #fff
+
 </style>
